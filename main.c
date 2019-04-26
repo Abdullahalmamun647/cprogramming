3a7dcd3c25400be41772775e5aa98d8852083fe6
Abdullah Al Mamun <39957854+Abdullahalmamun647@users.noreply.github.com>
2019-04-26 05:32:51 -0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int age;
+    printf("Enter your age");
+ scanf("%d",&age);
+ if (age>18)
+ {
+     printf("okk you are adult");
+
+
+ }
+ else if (age==18)
+              {
+        printf("you are perfect");
+     }
+else{
+        printf("You are restricted ");
+     }
+
+    return 0;
+}
